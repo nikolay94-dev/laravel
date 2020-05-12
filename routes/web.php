55b32c8daf34081fef343be6b3259d4f9a9fd7d0
nik-immelman <nikolay.kiselev@immelman.ru>
2020-05-12 10:09:21 +0300

    add new test route

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/laravel/test', function () {
+    return view('welcome');
+});
 Route::get('/render','MpdfController@pdfCreate')->name('all');
